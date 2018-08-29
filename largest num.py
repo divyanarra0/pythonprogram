3bb4248934ad0a8cefd3ecbaf8a591d7d1bcac50
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2018-08-29 03:27:16 -0700

    Update largest num.py

--- largest num.py
+++ largest num.py
@@ -1,10 +1,11 @@
-num1=int(input())
-num2=int(input())
-num3=int(input())
+num1=raw_input()
+num2=raw_input()
+num3=raw_input()
 if (num1>num2) and (num1>num3):
 	largest=num1
 elif (num2>num1) and (num2>num3):
 	largest=num2
 else:
 	largest=num3
-print(largest)
+print (largest)
+
