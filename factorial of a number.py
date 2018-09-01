3f037c5cee44b12f12711a22329919979aec6009
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2018-09-01 13:04:21 +0530

    Create factorial of a number.py

---
+++ factorial of a number.py
@@ -0,0 +1,10 @@
+num=int(input(""))
+factorial=1
+if num<0:
+	print("no")
+elif num == 0:
+	print("factorial of 0 is 1")
+else:
+	for i in range(1,num+1):
+		factorial=factorial*i
+	print(factorial)
