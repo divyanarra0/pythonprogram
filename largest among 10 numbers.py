6d3633839e9d967f40ed1b3b2839db61dea70e00
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2018-09-28 15:37:56 +0530

    Update largest among 10 numbers.py

--- largest among 10 numbers.py
+++ largest among 10 numbers.py
@@ -1,8 +1,7 @@
-# your code goes here 
 def max_num_in_list( list ):
     max = list[ 0 ]
     for a in list:
         if a > max:
             max = a
     return max
-print(max_num_in_list([1,2,14,15,17,13,24,88,77,66]))
+print(max_num_in_list([5,4,3,2,1,7,6,10,8,9]))
