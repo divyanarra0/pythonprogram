a6632bc010b1a6fadf66f3980f3c2affb3836efa
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2018-11-24 12:20:41 +0530

    Create median.py

---
+++ median.py
@@ -0,0 +1,9 @@
+l = int(raw_input())
+number = [1,2,3]
+number .sort()
+l = len(number)
+if (l  % 2 == 0):
+    median = (number[(l)//2] + number[(l)//2-1]) / 2
+else:
+    median = number[(l-1)//2]
+print(median)
