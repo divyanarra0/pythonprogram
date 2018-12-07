cb0029c9263b412a04c71003dc95622d0f2b08e2
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2018-12-07 12:30:25 +0530

    Create 108.py

---
+++ 108.py
@@ -0,0 +1,9 @@
+ def kthSmallest(arr, n, k): 
+    arr.sort() 
+    return arr[k-1] 
+if __name__=='__main__': 
+    arr = [1,2,3,4] 
+    n = len(arr) 
+    k = 2
+    print(kthSmallest(arr, n, k))
+  
