90dfc67711bc293cd57fd08c821f190461edc8fa
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2018-12-07 13:05:14 +0530

    Update 91.py

--- 91.py
+++ 91.py
@@ -1,5 +1,5 @@
 l,b,h=map(int,raw_input().split())
-area = 2 * (l * b + l * h + b * h)
-volume = l* b * h
-print(area),
-print(volume),
+area1 = 2 * (l * b + l * h + b * h)
+volume1 = l* b * h
+print(area1),
+print(volume1),
