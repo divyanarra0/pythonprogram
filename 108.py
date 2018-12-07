42fc0ac72135d53272eefa42e48eb5ca7d0f44a7
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2018-12-07 12:37:21 +0530

    Update 108.py

--- 108.py
+++ 108.py
@@ -2,8 +2,8 @@ def kthSmallest(arr, n, k):
     arr.sort() 
     return arr[k-1] 
 if __name__=='__main__': 
-    arr = [1,2,3,4] 
+    arr = [1,2,3,4,5] 
     n = len(arr) 
-    k = 2
+    k = 3
 print(kthSmallest(arr, n, k))
   
