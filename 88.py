74e7c1009c0ca6619093f79574c5130fd9d868f5
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2018-12-10 12:41:55 +0530

    Update 88.py

--- 88.py
+++ 88.py
@@ -1,10 +1,11 @@
-a,b=map(int,raw_input().split())
-if(a>b):
-    min1=a
+p1,q1=map(int,raw_input().split())
+if(p1>q1):
+    min1=p1
 else:
-    min1=b
+    min1=q1
 while(1):
-    if(min1%a==0 and min1%b==0):
+    if(min1%p1==0 and min1%q1==0):
         print(min1)
         break
     min1=min1+1
+
