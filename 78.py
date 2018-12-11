949132f85559e980718f0f1622a4e72d321f3aec
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2018-12-11 13:01:46 +0530

    Create 78.py

---
+++ 78.py
@@ -0,0 +1,38 @@
+def isMultipleOf13(n): 
+  
+    odd_count = 0
+    even_count = 0
+  
+    # Make no positive if +n is multiple of 13 
+    # then is -n. We are doing this to avoid 
+    # stack overflow in recursion 
+    if(n < 0):  
+        n = -n 
+    if(n == 0): 
+        return 1
+    if(n == 1):  
+        return 0
+  
+    while(n): 
+          
+        # If odd bit is set then 
+        # increment odd counter  
+        if(n & 1):  
+            odd_count += 1
+        n = n >> 1
+  
+        # If even bit is set then 
+        # increment even counter  
+        if(n & 1): 
+            even_count += 1
+        n = n >> 1
+  
+    return isMultipleOf13(abs(odd_count - even_count)) 
+  
+# Program to test function isMultipleOf13  
+num = 26
+if (isMultipleOf13(num)):  
+    print('no') 
+else: 
+    print('yes') 
+  
