2727d46a487478d55aded8d9a7149c12f30b7f9e
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2018-12-20 20:09:55 +0530

    Create p31.py

---
+++ p31.py
@@ -0,0 +1,19 @@
+def do_parentheses_match(input_string):
+    s = []
+    balanced = True
+    index = 0
+    while index < len(input_string) and balanced:
+        token = input_string[index]
+        if token == "(":
+            s.append(token)
+        elif token == ")":
+            if len(s) == 0:
+                balanced = False
+            else:
+                s.pop()
+
+        index += 1
+
+    print balanced and len(s) == 0
+print "yes"
+print "no"
