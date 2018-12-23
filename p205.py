896c9d421a837d7c40c71200ca45835d4df3f14e
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2018-12-23 20:43:08 +0530

    Create p205.py

---
+++ p205.py
@@ -0,0 +1,12 @@
+test = "HeLlo"
+ 
+def to_alternating_case(string):
+    result = ''
+    for word in string:
+        if word.isupper():
+            result += word.lower()
+        else:
+            result += word.upper()
+    return result
+ 
+print to_alternating_case(test)
