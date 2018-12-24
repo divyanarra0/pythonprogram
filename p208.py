c19fe5a056a0f34ee194df63798c3cbf21dda242
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2018-12-24 15:33:02 +0530

    Create p208.py

---
+++ p208.py
@@ -0,0 +1,8 @@
+matrix = [2,4],[3,7] #Assume a list is given
+total = 0
+for column in range(0, len(matrix[0])):
+        for row in range(0, len(matrix)):
+            total += matrix[row][column]
+        print("")
+        total = 0   #Reset total to zero before restarting count
+print 63
