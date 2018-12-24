58fa0b18e0e0084cc8e60351cb7c01ffb710bc71
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2018-12-24 14:14:30 +0530

    Create p206.py

---
+++ p206.py
@@ -0,0 +1,8 @@
+def Check():
+    while False:
+        for char in UserInput:
+            if char not in Valid:
+                print ('That is an invalid hex value.')
+        print('That is a valid hex value.')
+        return Check
+print "yes"
