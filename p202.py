c72d3d99aa93f3582f2507473cab8f24f8c9b81f
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2019-02-19 14:17:33 +0530

    Create p202.py

---
+++ p202.py
@@ -0,0 +1,32 @@
+def isVowel(c): 
+    c = c.lower() 
+    if (c == 'a' or c == 'e' or 
+        c == 'i' or c == 'o' or c == 'u'): 
+        return True
+    return False
+  
+# Function to return first X vowels 
+def firstXvowels(s, x): 
+      
+    # String to store first X vowels 
+    result = "" 
+    for i in range(0, len(s), 1): 
+          
+        # If s[i] is a vowel then 
+        # append it to the result 
+        if (isVowel(s[i])): 
+            result += s[i] 
+  
+        # If the desired length is reached 
+        if (len(result) == x): 
+            return result 
+      
+    # If total vowels are < X 
+    return "-1"
+  
+# Driver code 
+if __name__ == '__main__': 
+    str = "asdaqrew"
+    x = 3
+  
+    print(firstXvowels(str, x)) 
