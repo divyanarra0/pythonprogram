7a3a6d73d6c45cdfd6d4d6b53e968f1f91d56517
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2019-02-23 11:44:25 +0530

    Create p79.py

---
+++ p79.py
@@ -0,0 +1,14 @@
+def maxDiff(arr, arr_size): 
+    max_diff = arr[1] - arr[0] 
+      
+    for i in range( 0, arr_size ): 
+        for j in range( i+1, arr_size ): 
+            if(arr[j] - arr[i] > max_diff):  
+                max_diff = arr[j] - arr[i] 
+      
+    return max_diff 
+      
+# Driver program to test above function  
+arr = [1,2,3,4,5] 
+size = len(arr) 
+print ( maxDiff(arr, size)) 
