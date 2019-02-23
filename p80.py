5ae95903a3673bb7034cd04bbc68dd870016854d
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2019-02-23 11:51:54 +0530

    Update p80.py

--- p80.py
+++ p80.py
@@ -1,14 +1,14 @@
-def minDiff(arr, arr_size): 
-    min_diff = arr[1] - arr[0] 
+def maxDiff(arr, arr_size): 
+    max_diff = arr[1] - arr[0] 
       
     for i in range( 0, arr_size ): 
         for j in range( i+1, arr_size ): 
-            if(arr[j] - arr[i] < min_diff):  
-                min_diff = arr[j] - arr[i] 
+            if(arr[j] - arr[i] > max_diff):  
+                max_diff = arr[j] - arr[i] 
       
-    return min_diff 
+    return max_diff 
       
 # Driver program to test above function  
-arr = [1,2,3,4,5] 
+arr = [1, 2,3,4,2] 
 size = len(arr) 
-print ( minDiff(arr, size)) 
+print ( maxDiff(arr, size)) 
