e5bf09fbbeaa50f69ebd3f8f7015258cb43df8c1
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2019-03-04 15:41:27 +0530

    Create pro26.py

---
+++ pro26.py
@@ -0,0 +1,21 @@
+global maximum 
+def _lis(arr , n ): 
+    global maximum 
+    if n == 1 : 
+        return 1
+    maxEndingHere = 1
+    for i in xrange(1, n): 
+        res = _lis(arr , i) 
+        if arr[i-1] < arr[n-1] and res+1 > maxEndingHere: 
+            maxEndingHere = res +1
+    maximum = max(maximum , maxEndingHere) 
+    return maxEndingHere 
+def lis(arr): 
+    global maximum 
+    n = len(arr) 
+    maximum = 1
+    _lis(arr , n) 
+    return maximum 
+arr = [1,2,3,4,4,5,6,7,7,1] 
+n = len(arr) 
+print  lis(arr) 
