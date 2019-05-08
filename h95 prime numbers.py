8dc4b8d280bb71121351a738fd0dcfa123cf2468
divyanarra0 <42768718+divyanarra0@users.noreply.github.com>
2019-05-08 14:20:02 +0530

    Create h95 prime numbers.py

---
+++ h95 prime numbers.py
@@ -0,0 +1,14 @@
+def isPrime(n): 
+    if n < 1 : 
+        return False
+    for i in range(2, n): 
+        if n % i == 0: 
+            return False
+    return True
+def printPrime(n): 
+    for i in range(2, n + 1): 
+        if isPrime(i): 
+            print(i), 
+if __name__ == "__main__" : 
+    n = 10
+    printPrime(n) 
